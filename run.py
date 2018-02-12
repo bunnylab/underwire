781e3b18b674a520c8debf88ab483f2172de91d6
Graham Thompson <grahamwt42@gmail.com>
2018-02-12 14:45:30 -0800

    adds chat widget and widget switching

--- run.py
+++ run.py
@@ -1,64 +1,56 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QGridLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QGridLayout, QLabel, QLineEdit, QAction
 from PyQt5.QtWidgets import QTextEdit, QMessageBox, QStackedWidget, QHBoxLayout, QMainWindow
 
 # shift to messaging menu should have
-class ChatWindow(QWidget):
+class ChatWidget(QWidget):
 
-    def __init__(self):
-        super(ChatWindow, self).__init__(parent)
+    def __init__(self, parent=None):
+        super(ChatWidget, self).__init__(parent)
         self.initUI()
 
     def initUI(self):
-        # put some sort of scrolling text window
-        # put some sort of text input field
-        # put some sort of file menu
+        layout = QGridLayout()
 
+        self.chatHistory = QTextEdit()
+        self.chatInput = QLineEdit()
+        self.chatHistory.setReadOnly(True)
+        self.chatHistory.setLineWrapMode(QTextEdit.NoWrap)
 
+        layout.addWidget(self.chatHistory, 1, 0)
+        layout.addWidget(self.chatInput, 2, 0)
 
-class LoggedWidget(QWidget):
-    def __init__(self, parent=None):
-        super(LoggedWidget, self).__init__(parent)
-        layout = QHBoxLayout()
-        self.label = QLabel('logged in!')
-        layout.addWidget(self.label)
         self.setLayout(layout)
+
 
 
 # widget for logging into discord with token
 class LoginWidget(QWidget):
 
-    def testFunction(self):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-
-        msg.setText("This is a message box")
-        retval = msg.exec_()
-
     def __init__(self, parent=None):
         super(LoginWidget, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
         layout = QGridLayout()
 
-        handleSelfLabel = QLabel('Your Handle')
-        handleTargetLabel = QLabel('Target Handle')
-        discordTokenLabel = QLabel('Discord Token')
+        self.handleSelfLabel = QLabel('Your Handle')
+        self.handleTargetLabel = QLabel('Target Handle')
+        self.discordTokenLabel = QLabel('Discord Token')
 
-        handleSelfEdit = QLineEdit()
-        handleTargetEdit = QLineEdit()
-        discordTokenEdit = QLineEdit()
+        self.handleSelfEdit = QLineEdit()
+        self.handleTargetEdit = QLineEdit()
+        self.discordTokenEdit = QLineEdit()
 
-        connectButton = QPushButton('Connect', self)
-        connectButton.clicked.connect(self.testFunction)
+        self.connectButton = QPushButton('Connect', self)
 
-
-        layout.addWidget(handleSelfLabel, 1, 0)
-        layout.addWidget(handleSelfEdit, 1, 1)
-        layout.addWidget(handleTargetLabel, 2, 0)
-        layout.addWidget(handleTargetEdit, 2, 1)
-        layout.addWidget(discordTokenLabel, 3, 0)
-        layout.addWidget(discordTokenEdit, 3, 1)
-        layout.addWidget(connectButton, 5, 2)
-
+        layout.addWidget(self.handleSelfLabel, 1, 0)
+        layout.addWidget(self.handleSelfEdit, 1, 1)
+        layout.addWidget(self.handleTargetLabel, 2, 0)
+        layout.addWidget(self.handleTargetEdit, 2, 1)
+        layout.addWidget(self.discordTokenLabel, 3, 0)
+        layout.addWidget(self.discordTokenEdit, 3, 1)
+        layout.addWidget(self.connectButton, 5, 2)
 
         self.setLayout(layout)
 
@@ -74,15 +66,34 @@ class MainWindow(QMainWindow):
         self.central_widget = QStackedWidget()
         self.setCentralWidget(self.central_widget)
 
-        loggedwidget = LoggedWidget(self)
-        loginwidget = LoginWidget(self)
-        self.central_widget.addWidget(loggedwidget)
-        self.central_widget.addWidget(loginwidget)
-        self.central_widget.setCurrentWidget(loginwidget)
+        # exit setup thing
+        exitAct = QAction('&Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(app.quit)
+        # menubar
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+
+        # status bar
+        self.statusBar().showMessage('Ready')
+
+        self.initLoginWidget()
 
         self.setGeometry(600, 600, 500, 300)
         self.setWindowTitle('Underwire')
         self.show()
+
+    def initLoginWidget(self):
+        self.loginwidget = LoginWidget(self)
+        self.setCentralWidget(self.loginwidget)
+        self.loginwidget.connectButton.clicked.connect(self.initChatWidget)
+
+    def initChatWidget(self):
+        self.chatwidget = ChatWidget(self)
+        self.setCentralWidget(self.chatwidget)
+
 
 
 
